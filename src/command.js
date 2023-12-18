356eb24c0cc9a7926f1a0da52a957032c896d229
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-12-18 09:21:26 +0300

    Added first yargs commands

---
+++ src/command.js
@@ -0,0 +1,13 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+yargs(hideBin(process.argv))
+  .command('new <note>', 'Create new note', (yargs) => {
+    return yargs.positional('note', {
+        type : 'string',
+        description : "The content of the note to create",
+    })}, (argv) => {
+    console.log(argv.note)
+  })
+  .demandCommand(1)
+  .parse()
---
+++ utils.js
@@ -0,0 +1,5 @@
+ let num = 10;
+
+export const count = (num) => num;
+
+ export default num;
