6c4e14bb81269e49117b776e12dc11b4e9e35872
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-12-19 07:09:02 +0300

    Added findNotes, removeNotes, and delete all notes

--- src/notes.js
+++ src/notes.js
@@ -15,3 +15,23 @@ export const getAllNotes = async () =>{
     const  {notes} = getDB();
     return notes;
 }
+
+export const findNotes = async (filter) =>{
+    const {notes} = getDB();
+    return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()));
+}
+
+export const removeNote = async (id) =>{
+    const {notes} = await getDB();
+    const match = notes.find(note => note.id == id);
+
+    if(match){
+        const newNotes = notes.filter(note => note.id !== id);
+        await saveDB({notes: newNotes});
+        return id
+    }
+}
+
+export const removeNotes = async () =>{
+    await saveDB({notes: []})
+}
